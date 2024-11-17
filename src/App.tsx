77cdab8f7d4cc54cc86de78ae3f445be4f7b2348
Al Davis <al@teamaom.com>
2024-11-17 09:57:15 -0500

    Your commit message here

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Quiz from './components/Quiz'
 import Results from './components/Results'
 import Navbar from './components/Navbar'
+import QuizQuestions from './components/QuizQuestions'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Quiz />} />
+        <Route path="/quiz/:id" element={<QuizQuestions />} />
+        <Route path="/results/:id" element={<Results />} />
+      </Route>
+    ),
+    {
+      future: {
+        v7_startTransition: true,
+        v7_relativeSplatPath: true
+      }
+    }
+  )
+
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-gray-100">
         <Navbar />
         <main className="container mx-auto px-4 py-8">
           <Routes>
-            <Route path="/" element={<Quiz />} />
-            <Route path="/results/:id" element={<Results />} />
+            {router}
           </Routes>
         </main>
       </div>
