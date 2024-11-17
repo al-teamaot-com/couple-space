5b2302791476b1227383b9d2cbe92634eed7e38c
Al Davis <al@teamaom.com>
2024-11-17 05:39:14 -0500

    Your commit message here

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': {},
-      'process.env.NODE_ENV': JSON.stringify(mode),
-      'process.env.VITE_DATABASE_URL': JSON.stringify(env.VITE_DATABASE_URL),
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      process: path.resolve(__dirname, 'src/process.ts'),
+    },
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  }
+});
 });
