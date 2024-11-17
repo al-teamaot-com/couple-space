de5cc83ed03f4c056a17fc879c29c4a5b59b1f35
Al Davis <al@teamaom.com>
2024-11-17 09:44:58 -0500

    Your commit message here

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
 
-// For debugging
-console.log('Starting render...');
-
-try {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-  console.log('Render complete');
-} catch (error) {
-  console.error('Render failed:', error);
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
