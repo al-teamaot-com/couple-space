6cbcf8e749dc421b6e542c4b6e70f6960e9219ed
Al Davis <al@teamaom.com>
2024-11-17 08:02:02 -0500

    Your commit message here

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,27 @@ import * as React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-import Quiz from './pages/Quiz'
+
+// Wrap Quiz in error boundary
+const QuizWithError = () => {
+  try {
+    // Dynamically import Quiz
+    const Quiz = React.lazy(() => import('./pages/Quiz'));
+    return (
+      <React.Suspense fallback={<div className="p-4">Loading quiz...</div>}>
+        <Quiz />
+      </React.Suspense>
+    );
+  } catch (error) {
+    console.error('Quiz error:', error);
+    return (
+      <div className="p-4 bg-red-100">
+        <h1>Error loading quiz</h1>
+        <pre>{error?.message}</pre>
+      </div>
+    );
+  }
+};
 
 const App = () => {
   console.log('App component rendering...');
@@ -13,11 +33,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/quiz" element={
-            <div className="p-4">
-              <Quiz />
-            </div>
-          } />
+          <Route path="/quiz" element={<QuizWithError />} />
         </Routes>
       </div>
     </BrowserRouter>
