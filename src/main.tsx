987bde9fe99eb81facc615494e34e3bc4ed6a8f5
Al Davis <al@teamaom.com>
2024-11-17 06:53:42 -0500

    Your commit message here

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import ErrorBoundary from './components/ErrorBoundary';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
